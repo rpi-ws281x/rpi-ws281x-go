7e5bf66f71401d8f587a8e2493c9ded8fffc7b7f
Jacques Supcik <jacques.supcik@hefr.ch>
2017-08-18 23:48:13 +0200

    Cleanup + Add Circle CI

--- ws2811.go.h
+++ ws2811.go.h
@@ -1,4 +1,4 @@
-// Copyright 2016 Jacques Supcik / HEIA-FR
+// Copyright 2017 Jacques Supcik / HEIA-FR
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
